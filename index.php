33b150b6328bd1d8529f7ca35f4dbf49ff1fe387
Mxtthi <matthias.keintzel@gmail.com>
2022-05-23 23:11:37 +0200

    visual improvements

--- index.php
+++ index.php
@@ -39,6 +39,8 @@
         echo "<script>sessionStorage.setItem('loggedIn', false);;</script>";
     }
 
+    $data = [];
+
     $sql = "SELECT * FROM typingtest WHERE difficulty = 'easy' ORDER BY speed DESC";
     $stmt = $db->query($sql);
     $easy = $stmt->fetchAll();
@@ -51,56 +53,29 @@
     $stmt = $db->query($sql);
     $hard = $stmt->fetchAll();
 
+    array_push($data, $hard, $intermediate, $easy);
     echo "<div id='result'>";
 
-    echo "<p>TOP 10 Hard:</p>";
-    $count = 0;
-    $arr = [];
-    for ($i = 0; $i < count($hard); $i++) {
-        if (isset($hard[$i])) {
-            if (!($count < 10)) {
-                break;
-            }
-            if (!(in_array($hard[$i]["username"], $arr)) && $hard[$i]["userID"] != 0) {
-                array_push($arr, $hard[$i]["username"]);
-                $count++;
-                echo substr($hard[$i]["username"], 0, 32) . ": " . $hard[$i]["speed"] . " WPM | " . $hard[$i]["accuracy"] . "%" . "<br>";
+    for ($n = 0; $n < count($data); $n++) {
+
+        $count = 0;
+        $arr = [];
+
+        echo "<p>TOP 10 " . strtoupper($data[$n][0]['difficulty']) . ":</p>";
+
+        for ($i = 0; $i < count($data[$n]); $i++) {
+            if (isset($data[$n][$i])) {
+                if (!($count < 10)) {
+                    break;
+                }
+                if (!(in_array($data[$n][$i]["username"], $arr)) && $data[$n][$i]["userID"] != 0) {
+                    array_push($arr, $data[$n][$i]["username"]);
+                    $count++;
+                    echo $data[$n][$i]["speed"] . " WPM | " . $data[$n][$i]["accuracy"] . "% | " . substr($data[$n][$i]["username"], 0, 32) . "<br>";
+                }
             }
         }
     }
-
-    echo "<p>TOP 10 Intermediate:</p>";
-    $count = 0;
-    $arr = [];
-    for ($i = 0; $i < count($intermediate); $i++) {
-        if (isset($intermediate[$i])) {
-            if (!($count < 10)) {
-                break;
-            }
-            if (!(in_array($intermediate[$i]["username"], $arr)) && $intermediate[$i]["userID"] != 0) {
-                array_push($arr, $intermediate[$i]["username"]);
-                $count++;
-                echo substr($intermediate[$i]["username"], 0, 32) . ": " . $intermediate[$i]["speed"] . " WPM | " . $intermediate[$i]["accuracy"] . "%" . "<br>";
-            }
-        }
-    }
-
-    echo "<p>TOP 10 Easy:</p>";
-    $count = 0;
-    $arr = [];
-    for ($i = 0; $i < count($easy); $i++) {
-        if (isset($easy[$i])) {
-            if (!($count < 10)) {
-                break;
-            }
-            if (!(in_array($easy[$i]["username"], $arr)) && $easy[$i]["userID"] != 0) {
-                array_push($arr, $easy[$i]["username"]);
-                $count++;
-                echo substr($easy[$i]["username"], 0, 32) . ": " . $easy[$i]["speed"] . " WPM | " . $easy[$i]["accuracy"] . "%" . "<br>";
-            }
-        }
-    }
-
     echo "</div>";
 
     ?>
