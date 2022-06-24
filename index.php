be2260890a6ac60f837e24516b398710fc1db9c5
Mxtthi <matthias.keintzel@gmail.com>
2022-06-24 22:09:27 +0200

    final version

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script type="text/javascript" src="onloadScripts.js"></script>
     <script type="text/javascript" src="generateText.js"></script>
@@ -122,6 +123,12 @@
                 <hr>
             </div>
         </div>
+    </div>
+
+    <div id="home">
+        <a href="../../">
+            <button class="homeButton"><i class="fa fa-home"></i> Home</button>
+        </a>
     </div>
 
     <div id="main" class="unselectable">
